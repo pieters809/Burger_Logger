72af5633346e2680e99fe2f674dab3937c868b83
Jose Pieters <jose.pieters@Hotmail.com>
2020-03-30 20:36:24 -0400

    Database updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("50/50 Burger");
+INSERT INTO burgers (burger_name) VALUES ("Buffalo Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES("California Burger", true);
