4a754f4b2b198489ce3211f85e6759102b535d4f
Jose Pieters <jose.pieters@Hotmail.com>
2020-04-07 19:17:15 -0400

    working somewhat a little style added

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "burger_name", "devoured"
+    "name", "devoured"
   ], [
     req.body.name, false
   ], function(result) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
-INSERT INTO burgers (burger_name) VALUES ("50/50 Burger");
-INSERT INTO burgers (burger_name) VALUES ("Buffalo Burger");
-INSERT INTO burgers (burger_name, devoured) VALUES("California Burger");
+INSERT INTO burgers_db.burgers (name) VALUES ("Cheese Burger");
+INSERT INTO burgers_db.burgers (name) VALUES ("50/50 Burger");
+INSERT INTO burgers_db.burgers (name) VALUES ("Buffalo Burger");
+INSERT INTO burgers_db.burgers (name) VALUES ("California Burger");
